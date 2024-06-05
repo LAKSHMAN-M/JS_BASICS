d74dfcbf03401195cceddc12b3af929e58e536fa
Lakshman <lakshmanpersonals@gmail.com>
2024-06-05 16:38:41 +0530

    Data Types

---
+++ DataTypes.js
@@ -0,0 +1,25 @@
+// 1. String
+let str = "lakshman"
+console.log(str)
+// 2. Number
+let num = 10.3
+console.log(num)
+// 3. Object
+let obj = {
+    name:'Lakshman'
+}
+console.log(obj)
+console.log(obj["name"])
+console.log(obj.name)
+// 4. Symbol
+// 5. Boolean
+console.log(123 == 123)
+// 6. Null
+let a = null
+console.log(a)
+// 7. Undefined
+let b;
+console.log(b)
+// 8. BigInt
+let big_num = BigInt("123456789012345678901234567890")
+console.log(big_num)
